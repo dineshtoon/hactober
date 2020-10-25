2107a2d27e042752105312562bf67f7835e6b563
Dinesh <paliwaldinesh116@gmail.com>
2020-10-25 10:00:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+def Add(a,b):
+    return a+b
+def Subtract(a,b):
+    return a-b    
+def PrintTest():
+    return "Hello"
+
+"""
+a=float(input("Enter first number: "))
+b=float(input("Enter second number: "))   
+print("Addition of ",a," and ",b," is : ",Add(a,b))
+print("Subtraction of ",a," and ",b," is : ",Subtract(a,b)) 
+print(PrintTest())    
+"""    
